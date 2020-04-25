6a2dfe7b044ee596b4902357d3d48d93f3568f68
ixysec <1477821088@qq.com>
2020-04-25 13:23:23 +0800

    测试

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'Laot的笔记本',
+    description: 'Laot的笔记本，记录学习的技术知识。',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/favicon.ico`
+        }]
+    ],
+    dest: './docs/.vuepress/dist',
+    ga: '',
+    evergreen: true,
+}
